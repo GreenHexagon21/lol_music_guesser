cfe4713e7a92ec7787d7928f4145f2908c01039b
Hex <reppeti@gmail.com>
2023-10-02 14:58:28 +0200

    Added music files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestComponent } from './components/test/test.component';
+
 import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     MainComponent
   ],
   imports: [

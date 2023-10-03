1e309b42f3367541d2cbff7634970ef0b75844f0
Hex621 <reppeti@gmail.com>
2023-10-03 23:02:50 +0200

    Added tweaks

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -17,23 +17,42 @@ export class MainComponent {
   isPlaying:boolean = false;
   championsLocal:Champion[] = champions;
   selectedChampion:Champion;
+  currentChampNumb:number;
 
 
   constructor(public utils: UtilsService) {
 
   }
 
+  nextChampion() {
+    this.currentChampNumb = this.utils.randomIntFromInterval(1,champions.length);
+  }
+
   toggleMusic() {
+    if(!this.currentChampNumb) {
+      this.nextChampion();
+    }
     if(!this.isPlaying) {
-      var champNum = this.utils.randomIntFromInterval(1,champions.length);
-      let src = this.path+champions[champNum-1].name+this.ext
+      let src = this.path+champions[this.currentChampNumb-1].name+this.ext
       this.audio.src = src
       this.audio.load();
       this.audio.play();
+      console.log(this.audio.duration);
     } else {
       this.audio.pause();
     }
     this.isPlaying = !this.isPlaying
+  }
+
+  checkIfMatches() {
+
+    if(this.selectedChampion.name == champions[this.currentChampNumb-1].name) {
+      console.log("You won!");
+      this.toggleMusic();
+      this.nextChampion();
+    } else {
+      console.log('Not correct')
+    }
   }
 
 }
